1367a788d0946a3f166a081641e5668f78c34294
System Administrator <root@matt-usifers-macbook-pro.local>
2015-06-29 18:34:06 -0400

    Added comments

--- scripts/mattusifer.js
+++ scripts/mattusifer.js
@@ -2,12 +2,15 @@
 // Uses Ariel Flesler's jQuery.scrollTo plugin to simplify changing scroll position.
 
 $(document).ready(function() {
+
+	// hide everything
 	$('div.content').add('div.fill').hide();
+
+	//show first content box and project
 	$('div.content:first').show(); 
 	$('#project-one').show();
 
-	// includes
-	$("#piano-include").load("./piano/piano.html"); 
+	// include resume
 	$("div#two").load("./includes/resume.html");
 
 	// nav options onclick
@@ -15,9 +18,42 @@ $(document).ready(function() {
 	clickables.click(function() {
 		var activeTab = $(this).attr('title');
 
+		// if the click is on an inactive tab
 		if($('.activeNav a').attr('title') != activeTab) {
 
+			// stretch content to fill screen space
 			$('div.content').addClass('content-min-height');
+			 
+			// get current position, show all tabs
+			var currentPosition = $(document).scrollTop();
+			$('div.content').add('div.fill').show();
+			
+			// if we are already scrolled down
+			// set new active element
+			// get scroll offset
+			// reset document scrolltop to offset
+			if ($('.activeNav').length) {
+				var activeElement = $('.activeNav a').attr('title');
+				var scrollFrom = $(activeElement).offset().top + currentPosition-12;
+				$(document).scrollTop(scrollFrom);
+			}
+
+			// move .activeNav identifier to active tab
+			$('.activeNav').removeClass('activeNav'); 
+			$('a[title='+activeTab+']').parent().addClass('activeNav');
+					
+			// scroll
+	        $.scrollTo(activeTab, 1000, {easing:'swing', offset:-12});
+	        
+	        // after scroll is complete
+	        // hide everything except active tab
+	        // reset scrolltop
+	        // allow content to shrink to natural height
+			setTimeout(function() {
+				$('div.content').not(activeTab).add('div.fill').hide();
+				$(document).scrollTop(0);
+				$('div.content').removeClass('content-min-height');
+				}, 1050);
 
 			// scale the height of the skydive table
 			if (activeTab =='#three') {
@@ -25,61 +61,41 @@ $(document).ready(function() {
 					$('table.squares').attr('height',$('table.squares').width()*.625);
 				},500);
 			}
-			 
-			// get current position, show all tabs
-			var currentPosition = $(document).scrollTop();
-			$('div.content').add('div.fill').show();
-			
-			if ($('.activeNav').length) {
-				var activeElement = $('.activeNav a').attr('title');
-				var scrollFrom = $(activeElement).offset().top + currentPosition-12;
-				$(document).scrollTop(scrollFrom);
-			}
-
-			$('.activeNav').removeClass('activeNav'); 
-			$('a[title='+activeTab+']').parent().addClass('activeNav');
-					
-	        $.scrollTo(activeTab, 1000, {easing:'swing', offset:-12});
-	        
-			setTimeout(function() {
-				$('div.content').not(activeTab).add('div.fill').hide();
-				$(document).scrollTop(0);
-				$('div.content').removeClass('content-min-height');
-				}, 1050);
 		}
 	});
 
-	// project tabs
+	// show project content on hover
 	$('ul#project-tabs a').hover(function() {
 		var currentProj = $(this).attr('name');
-
 		$('.activeProj').removeClass('activeProj');
 		$(this).parent().addClass('activeProj');
-
 		$('div.project-content').hide();
 		$('div#' + currentProj).show();
 	});
 
+	// set skydive table to resize
 	$(window).resize(function() {
 		$('table.squares').attr('height',$('table.squares').width()*.625);
 	});
 
-	// piano
+	// resize piano when the project content loads
 	$('ul#project-tabs a[name="project-map"]').hover(mapResize);
 
-	var channel_max = 20;										// number of channels
+	// set up audio channels for piano
+	var channel_max = 20;
 	audiochannels = new Array();
-	for (a=0;a<channel_max;a++) {									// prepare the channels
+	for (a=0;a<channel_max;a++) {
 		audiochannels[a] = new Array();
-		audiochannels[a]['channel'] = new Audio();						// create a new audio object
-		audiochannels[a]['finished'] = -1;							// expected end time for this channel
+		audiochannels[a]['channel'] = new Audio();	// new audio
+		audiochannels[a]['finished'] = -1;	// no end time
 	}
 });
 
+// plays notes through audio channels
 function playNotes(s) {
 	for (a=0;a<audiochannels.length;a++) {
 		thistime = new Date();
-		if (audiochannels[a]['finished'] < thistime.getTime()) {			// is this channel finished?
+		if (audiochannels[a]['finished'] < thistime.getTime()) {	// check if channel is finished
 			audiochannels[a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000;
 			audiochannels[a]['channel'].src = document.getElementById(s).src;
 			audiochannels[a]['channel'].load();
@@ -89,41 +105,64 @@ function playNotes(s) {
 	}
 }
 
-/*
-// imageMapResize
-var originalMapHTML;
-var mapId;
-
-$(document).ready(function() {
-	mapId = $('img[name="map-resize"]').attr('usemap');
-	originalMapHTML = $('<map>').append($(mapId).clone()).html();
-
-	$(window).resize(function() {
-		$('table.squares').attr('height',$('table.squares').width()*.625);
-
-	    if(this.resizeTO) clearTimeout(this.resizeTO);
-	    this.resizeTO = setTimeout(function() {
-	        $(this).trigger('resizeEnd');
-	    }, 500);
-	});
-
-	$(window).bind('resizeEnd', mapResize);
-	$('ul#project-tabs a[name="project-map"]').hover(mapResize);
-});
-
-function mapResize() {
-	var mapImg = $('img[name="map-resize"]');
-	var widthFactor = mapImg.width()/1343;
-	var originalMapAreas = $(originalMapHTML).find('area');
-	var currentMapAreas = $(mapId + ' area');
-
-	for(var i =0; i<currentMapAreas.length; i++) {
-		var originalCoords = originalMapAreas[i].coords.split(',');
-		var newCoords = currentMapAreas[i].coords.split(',');
-		for(var j=0;j<newCoords.length;j++) {
-			newCoords[j] = originalCoords[j]*widthFactor;
-		}
-
-		currentMapAreas[i].coords = newCoords.join(',');
-	}
-} */
+// functions to highlight active piano keys
+function C()
+{
+document.getElementById("piano").src="./piano/pictures/c.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function Csharp()
+{
+document.getElementById("piano").src="./piano/pictures/csharp.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function D()
+{
+document.getElementById("piano").src="./piano/pictures/d.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function Dsharp()
+{
+document.getElementById("piano").src="./piano/pictures/dsharp.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function E()
+{
+document.getElementById("piano").src="./piano/pictures/e.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function F()
+{
+document.getElementById("piano").src="./piano/pictures/f.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function Fsharp()
+{
+document.getElementById("piano").src="./piano/pictures/fsharp.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function G()
+{
+document.getElementById("piano").src="./piano/pictures/g.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function Gsharp()
+{
+document.getElementById("piano").src="./piano/pictures/gsharp.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function A()
+{
+document.getElementById("piano").src="./piano/pictures/a.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function Asharp()
+{
+document.getElementById("piano").src="./piano/pictures/asharp.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
+function B()
+{
+document.getElementById("piano").src="./piano/pictures/b.jpg";
+setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
+}
