b40c7a1aac879af4f54ec00a69c33f6fbb372672
Matt Usifer <mattusifer@gmail.com>
2015-06-29 09:40:40 -0400

    Fixed piano

--- scripts/mattusifer.js
+++ scripts/mattusifer.js
@@ -64,31 +64,30 @@ $(document).ready(function() {
 		$('table.squares').attr('height',$('table.squares').width()*.625);
 	});
 
-	// only show piano in firefox
-	// TODO: figure out how to make it work in other browsers
-	navigator.sayswho= (function(){
-	    var ua= navigator.userAgent, tem,
-	    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-	    if(/trident/i.test(M[1])){
-	        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-	        return 'IE '+(tem[1] || '');
-	    }
-	    if(M[1]=== 'Chrome'){
-	        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-	        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-	    }
-	    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-	    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-	    return M.join(' ');
-	})();
+	// piano
+	$('ul#project-tabs a[name="project-map"]').hover(mapResize);
 
-	if(navigator.sayswho.match(/^firefox/i)) {
-		$('#piano-div').show();
-		$('ul#project-tabs a[name="project-map"]').hover(mapResize);
+	var channel_max = 20;										// number of channels
+	audiochannels = new Array();
+	for (a=0;a<channel_max;a++) {									// prepare the channels
+		audiochannels[a] = new Array();
+		audiochannels[a]['channel'] = new Audio();						// create a new audio object
+		audiochannels[a]['finished'] = -1;							// expected end time for this channel
 	}
-
-
 });
+
+function playNotes(s) {
+	for (a=0;a<audiochannels.length;a++) {
+		thistime = new Date();
+		if (audiochannels[a]['finished'] < thistime.getTime()) {			// is this channel finished?
+			audiochannels[a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000;
+			audiochannels[a]['channel'].src = document.getElementById(s).src;
+			audiochannels[a]['channel'].load();
+			audiochannels[a]['channel'].play();
+			break;
+		}
+	}
+}
 
 /*
 // imageMapResize
