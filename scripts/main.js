2992ab37816cd9fa3bfa15c3486f915b23a91f27
Matt Usifer <mattusifer@gmail.com>
2015-09-24 18:42:22 -0400

    Updated location

--- scripts/main.js
+++ scripts/main.js
@@ -65,8 +65,42 @@ $(document).ready(function() {
     }
   });
 
+  // get info from github API
+  var url='https://api.github.com/users/mattusifer/repos',
+    $mainContainer = $('#one'),
+    $liContainer = $('ul#project-tabs'),
+    $liTemplate = $('li.hidden'),
+    $contentTemplate = $('div.project-content'),
+    $liCurrentTemplate;
+
+  $.getJSON(url, function(data, textStatus) {
+    data.sort(function(a,b) {
+      return (new Date(b.updated_at) < new Date(a.updated_at)) ? -1 : 1;
+    });
+
+    for (i=0; i<data.length; i++) {
+      repo = data[i];
+      $liCurrentTemplate = $liTemplate.clone();
+      $contentCurrentTemplate = $contentTemplate.clone();
+
+      if (repo.fork) continue;
+
+      $liCurrentTemplate.find('a').text(repo.name).
+        attr('href',repo.html_url).
+        attr('name',repo.name);
+
+      if (repo.language) $liCurrentTemplate.find('span').text(repo.language);
+
+      $contentCurrentTemplate.text(repo.description).
+        attr('id',repo.name);
+
+      $liContainer.append($liCurrentTemplate.removeClass('hidden'));
+      $mainContainer.append($contentCurrentTemplate);
+    }
+  });
+
   // show project content on hover
-  $('ul#project-tabs a').hover(function() {
+  $('ul#project-tabs').on("mouseenter","a",function() {
     var currentProj = $(this).attr('name');
     $('.activeProj').removeClass('activeProj');
     $(this).parent().addClass('activeProj');
@@ -74,131 +108,9 @@ $(document).ready(function() {
     $('div#' + currentProj).show();
   });
 
-  // map-resize
-  // get image map id
-  imgMapId = $('img.map-resize').attr('usemap');
-
-  // set original map html to save the original coordinates
-  originalMapHTML = $('<map>').append($(imgMapId).clone()).html();
-
   // set to trigger at the end of a window resize
   $(window).resize(function() {
-
     // resize skydive table
     $('table.squares').attr('height',$('table.squares').width() * 0.625);
-
-    // resize piano
-      if(this.resizeTO) clearTimeout(this.resizeTO);
-      this.resizeTO = setTimeout(function() {
-          $(this).trigger('resizeEnd');
-      }, 500);
   });
-  $(window).bind('resizeEnd', mapResize);
-
-  // resize piano when the project content loads
-  $('ul#project-tabs a[name="project-map"]').hover(mapResize);
-
-  // set up audio channels for piano
-  var channel_max = 20;
-  audiochannels = new Array();
-  for (a=0;a<channel_max;a++) {
-    audiochannels[a] = new Array();
-    audiochannels[a]['channel'] = new Audio();  // new audio
-    audiochannels[a]['finished'] = -1;  // no end time
-  }
 });
-
-// play notes through audio channels
-function playNotes(s) {
-  for (a=0;a<audiochannels.length;a++) {
-    thistime = new Date();
-    if (audiochannels[a]['finished'] < thistime.getTime()) {  // check if channel is finished
-      audiochannels[a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000;
-      audiochannels[a]['channel'].src = document.getElementById(s).src;
-      audiochannels[a]['channel'].load();
-      audiochannels[a]['channel'].play();
-      break;
-    }
-  }
-}
-
-// function for map-resize
-function mapResize() {
-  var mapImg = $('img.map-resize');
-  var widthFactor = mapImg.width()/imgMapWidth;
-  var originalMapAreas = $(originalMapHTML).find('area');
-  var currentMapAreas = $(imgMapId + ' area');
-
-  // remap coords
-  for(var i =0; i<currentMapAreas.length; i++) {
-    var originalCoords = originalMapAreas[i].coords.split(',');
-    var newCoords = currentMapAreas[i].coords.split(',');
-    for(var j=0;j<newCoords.length;j++) {
-      newCoords[j] = originalCoords[j]*widthFactor;
-    }
-
-    currentMapAreas[i].coords = newCoords.join(',');
-  }
-}
-
-// functions to highlight active piano keys
-function C()
-{
-document.getElementById("piano").src="./piano/pictures/c.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function Csharp()
-{
-document.getElementById("piano").src="./piano/pictures/csharp.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function D()
-{
-document.getElementById("piano").src="./piano/pictures/d.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function Dsharp()
-{
-document.getElementById("piano").src="./piano/pictures/dsharp.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function E()
-{
-document.getElementById("piano").src="./piano/pictures/e.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function F()
-{
-document.getElementById("piano").src="./piano/pictures/f.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function Fsharp()
-{
-document.getElementById("piano").src="./piano/pictures/fsharp.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function G()
-{
-document.getElementById("piano").src="./piano/pictures/g.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function Gsharp()
-{
-document.getElementById("piano").src="./piano/pictures/gsharp.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function A()
-{
-document.getElementById("piano").src="./piano/pictures/a.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function Asharp()
-{
-document.getElementById("piano").src="./piano/pictures/asharp.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
-function B()
-{
-document.getElementById("piano").src="./piano/pictures/b.jpg";
-setTimeout('document.getElementById("piano").src="./piano/pictures/piano.jpg"',200);
-}
